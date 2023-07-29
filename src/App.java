fd256161b41d51b6a740fbcf886e56487b087911
Josue Oliveira <josue55471@yahoo.com>
2023-07-29 10:37:25 -0300

    initial commit

---
+++ src/App.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+        String tipoDeOperacao;
+        double valor1, valor2, resultado;
+
+        System.out.println("Tipos de operação: + (Adição), - (Subtração), * (Multiplicação), / (Divisão) ");
+        System.out.print("Indique o tipo de operação, conforme a mensagem acima: ");
+        tipoDeOperacao = ler.nextLine();
+        //teste
+        if(tipoDeOperacao != "+"){
+            System.out.println("Favor inserir um tipo de operação válido");
+        }else if (tipoDeOperacao != "-"){
+            System.out.println("Favor inserir um tipo de operação válido");
+        }else if(tipoDeOperacao != "*"){
+            System.out.println("Favor inserir um tipo de operação válido");
+        }else if(tipoDeOperacao != "/"){
+            System.out.println("Favor inserir um tipo de operação válido");  
+        }
+
+
+        System.out.print("Insira um valor: ");
+        valor1 = ler.nextDouble();
+        System.out.print("Insira um segundo valor: ");
+        valor2 = ler.nextDouble();
+
+        ler.close();
+
+        switch (tipoDeOperacao)
+        {
+            case "+":
+                resultado = valor1 + valor2;
+                System.out.println("O resultado é: " + resultado);
+                break;
+
+            case "-":
+                resultado = valor1 - valor2;
+                System.out.println("O resultado é: " + resultado);
+                break;
+
+            case "*":
+                resultado = valor1 * valor2;
+                System.out.println("O resultado é: " + resultado);
+                break;
+
+            case "/":
+                resultado = valor1 / valor2;
+                System.out.println("O resultado é: " + resultado);
+                break;
+            default:
+                System.out.println("Insira um tipo de operação válido");
+        }
+    }    
+}
+
